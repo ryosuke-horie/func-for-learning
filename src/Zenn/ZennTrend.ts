251f9830b3bc9316cbfcca9a7ad2e1d128463a87
EC2 Default User <ec2-user@ip-172-31-33-195.ap-northeast-1.compute.internal>
2023-02-24 04:08:21 +0000

    zenn追加

--- src/Zenn/ZennTrend.ts
+++ src/Zenn/ZennTrend.ts
@@ -5,9 +5,6 @@ type APIResponse = {
     path: string
 }
 
-// https://qiita.com/HelloRusk/items/803f9599cde72810f1a8
-// 上記の記事で公開されているAPIを利用。
-// Qiitaのトレンド記事をスクレイピングしたものをJSONで返すもの。
 const ZennTrendURL: string = 'https://zenn-api.vercel.app/api/trendTech'
 
 // Qiitaトレンドを取得し、メール本文用の文章に変換する。
@@ -19,7 +16,6 @@ const getZennTrend = async(): Promise<any> => {
         mailBodyText = mailBodyText + element.title + '\br (' + element.path + ')' + '\n'
         mailBodyText = mailBodyText + '---------------- \n \n'
     });
-
     return mailBodyText
 }
 
--- src/Zenn/index.ts
+++ src/Zenn/index.ts
@@ -1,12 +1,14 @@
-import getQiitaTrend from "./ZennTrend";
+import getZennTrend from "./ZennTrend";
 import sendMail from "./SendMail";
 
 // 実行
 // Qiitaトレンドを取得し、Gmailで送信する。
 export async function handler(): Promise<any> {
     // メール送信用の文章を取得
-    const mailBodyText = getQiitaTrend()
+    const mailBodyText = getZennTrend()
+    console.log(mailBodyText)
 
     // メール送信処理
     return sendMail(await mailBodyText)
 }
+handler();
